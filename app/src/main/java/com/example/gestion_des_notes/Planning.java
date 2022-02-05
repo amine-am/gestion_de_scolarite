325fcbcda7d5305eb91be60b2b7dc04e81b24035
amine-am <aminealaouim98@gmail.com>
2022-02-05 19:07:09 +0100

    Planning Changes

--- app/src/main/java/com/example/gestion_des_notes/Planning.java
+++ app/src/main/java/com/example/gestion_des_notes/Planning.java
@@ -3,14 +3,23 @@ package com.example.gestion_des_notes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.sql.SQLOutput;
 
 public class Planning extends AppCompatActivity implements View.OnClickListener {
 
     public CardView c;
-
+    Spinner spinnerfil;
+    String arrfil[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +27,20 @@ public class Planning extends AppCompatActivity implements View.OnClickListener 
 
         c = (CardView) findViewById(R.id.planning);
         c.setOnClickListener(this);
-
+        spinnerfil = (Spinner) findViewById(R.id.inputfiliere);
+        SQLiteDatabase db = openOrCreateDatabase("myDB", Context.MODE_PRIVATE,null);
+        Cursor c = db.rawQuery("SELECT * FROM filiers", null);
+        arrfil = new String[c.getCount()];
+        c.moveToFirst();
+        for (int i=0;i< arrfil.length;i++){
+            arrfil[i] = c.getString(1);
+            System.out.println("THE DATAS");
+            System.out.println(i);
+            System.out.println(arrfil[i]);
+            c.moveToFirst();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrfil);
+        spinnerfil.setAdapter(adapter);
     }
 
     @Override
@@ -26,5 +48,8 @@ public class Planning extends AppCompatActivity implements View.OnClickListener 
         Intent i = new Intent(this, AddPlanning.class);
         startActivity(i);
 
+    }
+    private void showToast(String text){
+        Toast.makeText(Planning.this, text, Toast.LENGTH_SHORT).show();
     }
 }
